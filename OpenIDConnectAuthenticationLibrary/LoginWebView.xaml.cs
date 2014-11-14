ed6f3a235de61e5a44daabbd14968721e4b7cfd4
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-11-14 22:34:27 +0100

    initial

---
+++ OpenIDConnectAuthenticationLibrary/LoginWebView.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using Thinktecture.IdentityModel.Client;
+
+namespace Thinktecture.Samples
+{
+    public partial class LoginWebView : Window
+    {
+        public AuthorizeResponse AuthorizeResponse { get; set; }
+        public event EventHandler<AuthorizeResponse> Done;
+        public TaskCompletionSource<AuthorizeResponse> _source = new TaskCompletionSource<AuthorizeResponse>();
+        Uri _callbackUri;
+
+        private string _clientid;
+
+        public LoginWebView(string clientid)
+        {
+            InitializeComponent();
+            webView.Navigating += webView_Navigating;
+
+            Closing += LoginWebView_Closing;
+            _clientid = clientid;
+        }
+        public Task<AuthorizeResponse> Completion
+        {
+            get
+            {
+                return _source.Task;
+            }
+        }
+        void LoginWebView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+          //  e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+            if(!_source.Task.IsCompleted)
+                _source.SetResult(null);
+        }
+
+        public void Start(Uri startUri, Uri callbackUri)
+        {
+            _callbackUri = callbackUri;
+            webView.Navigate(startUri);
+        }
+
+        private void webView_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Uri.ToString().StartsWith(_callbackUri.AbsoluteUri))
+            {
+                AuthorizeResponse = new AuthorizeResponse(e.Uri.AbsoluteUri);
+
+                //e.Cancel = true;
+                this.Visibility = System.Windows.Visibility.Hidden;
+               
+                if (Done != null)
+                {
+                    Done.Invoke(this, AuthorizeResponse);
+                }
+                _source.SetResult(AuthorizeResponse);
+                 this.Close();
+            }
+        }
+
+        public void RequestToken(string authuri, string scope, string responseType)
+        {
+            var additional = new Dictionary<string, string>
+            {
+                { "nonce", "nonce" },
+                // { "login_hint", "idp:Google" }
+            };
+
+            var client = new OAuth2Client(new Uri(authuri));
+            var startUrl = client.CreateAuthorizeUrl(
+               _clientid,
+                responseType,
+                scope,
+                "oob://localhost/wpfclient",
+                "state",
+                additional);
+
+
+            this.Show();
+            this.Start(new Uri(startUrl), new Uri("oob://localhost/wpfclient"));
+        }
+    }
+}
---
+++ TestConsole for Authentication Library/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenIDConnectAuthenticationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Thinktecture.Samples;
+
+namespace TestConsole_for_Authentication_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //,"data.read data.write sub.read", "token"
+
+            var authContext = new OpenIDConnectContext("https://identity-staging.cloudapp.net/connect/authorize", "6994A4A8-0E65-4FED-A82B-C684A0DD1758");
+
+            var result = authContext.RequestTokenAsync("data.read data.write sub.read", "token");
+
+            var test = result.Result;
+           
+        }
+
+      
+    }
+}
