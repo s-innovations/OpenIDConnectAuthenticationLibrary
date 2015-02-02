5f35665f94c2107a4a06d42f9b6c092486e2e337
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-02-02 05:33:35 +0100

    updated dependencies

--- OpenIDConnectAuthenticationLibrary/LoginWebView.xaml.cs
+++ OpenIDConnectAuthenticationLibrary/LoginWebView.xaml.cs
@@ -77,7 +77,7 @@ namespace Thinktecture.Samples
                 scope,
                 callbackUri,
                 "state",
-                additional);
+                additionalValues: additional);
 
 
             this.Show();
